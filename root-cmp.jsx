b015738890d7596add95739b436585e006c4a975
InbarSabag <pe.inbar@gmail.com>
2022-08-24 11:14:09 +0300

    first upload

---
+++ root-cmp.jsx
@@ -0,0 +1,24 @@
+import { AppHeader } from "./cmps/app-header.jsx"
+import { About } from "./views/about.jsx"
+import { Home } from "./views/home.jsx"
+import { MailIndex } from "./apps/mail/views/mail-index.jsx"
+import { NoteIndex } from "./apps/note/views/note-index.jsx"
+import { UserMsg } from "./cmps/user-msg.jsx"
+
+const Router = ReactRouterDOM.HashRouter
+const { Route, Switch } = ReactRouterDOM
+
+export function App() {
+    return <Router>
+        <section className="app">
+            <AppHeader />
+            <Switch>
+                <Route path="/mail" component={MailIndex} />
+                <Route path="/note" component={NoteIndex} />
+                <Route path="/about" component={About} />
+                <Route path="/" component={Home} />
+            </Switch>
+        </section>
+        <UserMsg/>
+    </Router>
+}
