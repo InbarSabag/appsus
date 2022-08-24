4f8a25d27e7e82fa5f0a00af9b0490120debb472
InbarSabag <pe.inbar@gmail.com>
2022-08-24 22:25:04 +0300

    "get notes"

--- apps/note/cmps/note-list.jsx
+++ apps/note/cmps/note-list.jsx
@@ -1,8 +1,14 @@
-import { NotPreview } from '../cmps/note-preview.jsx'
+// import { NotePreview } from '../cmps/note-preview.jsx'
 
 export class NoteList extends React.Component {
 
+state={
+    notes: [1,2,3]
+}
+
+
+
 render(){
-return 
+return <h1>list</h1>
 }
 }
