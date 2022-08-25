4b2ec04a4ad0e40202181cc4d82b0a5f32935b66
OzZaken <ozzaken0201@gmail.com>
2022-08-25 11:09:31 +0300

    showTime At MailPreview

--- apps/mail/cmps/mail-preview.jsx
+++ apps/mail/cmps/mail-preview.jsx
@@ -1,10 +1,18 @@
 export function MailPreview({ isRead, subject, body, sentAt }) {
     const readClass = isRead ? 'read' : ''
-    return ( <tr className="mail-preview">
-            <td><input type="checkbox" name="" id="" /></td>
-            <td>{subject}</td>
-            <td>{body}</td>
-            <td>{Date(sentAt)}</td>
-        </tr>
+    return (<tr className="mail-preview">
+        <td><input type="checkbox" name="" id="" /></td>
+        <td>{subject}</td>
+        <td>{body}</td>
+        <td>{showTime(sentAt)}</td>
+    </tr>
     )
+}
+
+function showTime(sentAt) {
+    const diff = Date.now() - sentAt;
+    const day = 24 * 60 * 60 * 1000
+    const date = new Date(sentAt)
+    if (diff < day) return `${date.getHours()}:${date.getMinutes()}`
+    else return `${date.getDate()} ${date.toLocaleString('en', { month: 'short' })}`
 }
